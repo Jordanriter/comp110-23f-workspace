45bdc12b67e3ea90660b43ad8904537e493c2018
Jordan Riter <jojor@ad.unc.edu>
2023-11-16 11:00:08 -0500

    progress

--- exercises/ex08/fish.py
+++ exercises/ex08/fish.py
@@ -41,10 +41,24 @@ class Fish:
 =======
 """Fish Class."""
 
+__author__ = "730388067"
+
 
 class Fish:
+    """Class to represent fish in river."""
+
     def __init__(self, age: int = 0):
         """Represents fish."""
         self.age = age
+<<<<<<< HEAD
 >>>>>>> ca16449 (progress)
+<<<<<<< HEAD
 >>>>>>> a515408 (progress)
+=======
+=======
+
+    def one_day(self):
+        """Increase the value of the age attribute by 1."""
+        self.age += 1
+>>>>>>> 89d9dc1 (progress)
+>>>>>>> 95f4f39 (progress)
--- exercises/ex08/river_simulation.py
+++ exercises/ex08/river_simulation.py
@@ -1,6 +1,7 @@
 """River Simulation."""
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 __author__ = "730388067"
 
 from exercises.ex08.river import River
@@ -14,12 +15,23 @@ def main():
     my_river.view_river()
 =======
 from river import River
+=======
+__author__ = "730388067"
+
+from exercises.ex08.river import River
+>>>>>>> 95f4f39 (progress)
 
 
 def main():
+    """Main function."""
     my_river = River(num_fish=10, num_bears=2)
     my_river.view_river()
+<<<<<<< HEAD
 >>>>>>> a515408 (progress)
+=======
+    my_river.one_river_week()
+    my_river.view_river()
+>>>>>>> 95f4f39 (progress)
 
 
 if __name__ == "__main__":
